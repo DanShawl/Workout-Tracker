6b365e6fab3ea6c1fb3b469d8abcd347bae04fed
Dan Shawl <danielrshawl@gmail.com>
2021-08-24 07:55:01 -0400

    Added core front-end functionality to the todo app including appending items to the todo list. Also added material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import { useState } from 'react';
 import './App.css';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 
 function App() {
-  const [todos, setTodos] = useState([
-    'Take dogs for walk',
-    'Take the rubbish out',
-  ]);
-  const [input, setInput] = useState('Add Item');
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+    console.log(todos);
+  };
+
   return (
     <div className="App">
       <h1>To Do</h1>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        onClick={(e) => setInput('')}
-      />
-      <button>Add Todo</button>
-      <h1>{input}</h1>
+
+      <FormControl>
+        <InputLabel>Add ToDo</InputLabel>
+        <Input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </FormControl>
+      <Button
+        type="submit"
+        onClick={addTodo}
+        disabled={!input}
+        variant="contained"
+        color="primary"
+        disableElevation
+      >
+        Add Todo
+      </Button>
+
+      {/* <form action="">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button
+          type="submit"
+          onClick={addTodo}
+          disabled={!input}
+          variant="contained"
+          color="primary"
+          disableElevation
+        >
+          Add Todo
+        </Button>
+      </form> */}
 
       <ul>
         {todos.map((todo) => {
@@ -29,3 +64,10 @@ function App() {
 }
 
 export default App;
+
+//  Building the Todo App
+//  1.  Create an input and button with a ul
+//  2.  Use state to manage the todo list
+//  3.  Map over the todo list to create a list of li items
+//  4.  Use state to manage user input and setup a button click event
+//  5.  Push input to todos
